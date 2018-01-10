cd03fb135cfc2e96b7e2e8d649d45503de4b5d1c
Stiixxy <veenman.stijn@gmail.com>
2018-01-10 11:21:12 +0100

    Finished basic chat

--- chatController.js
+++ chatController.js
@@ -3,15 +3,14 @@ module.exports = function(io){
 	console.log("Chat controller started running");
 
 	io.on('connection', function(socket){
-		socket.on('message', function(message){
-			console.log(`Received ${message}`);
-	
-		   
-		});
-		socket.emit('message', 'We received the message, hello to you to!');
 		socket.on('disconnect', function(){
-			console.log("User dc");
+			console.log("User disconnected");
 		});
+
+		socket.on('sendMessage', function(message){
+			socket.broadcast.emit('receivedMessage', message);
+		});
+
 		console.log(`User ${socket.id} connected`);
 	});
 
--- public/socket.js
+++ public/socket.js
@@ -1,6 +1,29 @@
-var socket = io('localhost:80');
-socket.emit('message', 'Hi i am a client');
+var socket = io();
 
-socket.on('message', function(message){
-    console.log(message);
+function enterPressed(event){
+    if(event.keycode == 13 || event.which == 13){
+        var textBox = document.getElementById('messageBox');
+        var messageText = textBox.value.trim();
+        if(messageText.length > 0){
+            //Send the message to the server and clear textbox
+            socket.emit("sendMessage", messageText);
+            textBox.value = "";
+
+            //Add our own message to the box
+            addMessage(messageText, "ourMessage");
+        }
+    }
+}
+
+function addMessage(message, whos){
+    var chatItem = document.createElement('div');
+    chatItem.classList.add('message');                    
+    chatItem.classList.add(whos);
+    chatItem.innerHTML=message;
+    document.getElementById('chatBox').appendChild(chatItem);
+}
+
+socket.on('receivedMessage', function(message){
+    //message from somone else
+    addMessage(message, "theirMessage")
 });
